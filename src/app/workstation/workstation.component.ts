64a9fa67692eb7909556bbfaaddf982375363014
maratree <maratsup@metrosystems.co.th>
2017-09-19 15:55:05 +0700

    upload

--- src/app/workstation/workstation.component.ts
+++ src/app/workstation/workstation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-workstation',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkstationComponent implements OnInit {
 
-  constructor() { }
+  chkAll = "";
+  currentLang = "en";
+  constructor(private tranService: TranslateService, private router: Router) { }
 
   ngOnInit() {
+    this.tranService.use(this.currentLang);
+    $(document).ready(function () {
+      $('.materialboxed').materialbox();
+    });
+    $(document).ready(function () {
+      $('ul.tabs').tabs();
+    });
+    $(document).ready(function () {
+      $('select').material_select();
+    });
+
+  }
+  chkOutletAll() {
+    this.chkAll = "checked"
+  }
+  chkOutletUnAll() {
+    this.chkAll = ""
   }
 
 }
